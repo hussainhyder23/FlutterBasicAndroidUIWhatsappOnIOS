60fadd6bd84bc70be6b21e2f3d1b2d3bc72ac2d8
hussainhyder23 <hussainhyder23@gmail.com>
2020-03-08 18:40:46 +0530

    first commit

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,53 @@
+class ChatModel{
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name,this.message,this.time,this.avatarUrl});
+}
+  List <ChatModel> dummyData = [
+    new ChatModel(
+      name: "Hyder",
+      message: "Heyyy Thereee",
+      time: "15:30",
+      avatarUrl: "https://cdn.discordapp.com/attachments/588757704948973604/686197258731126854/IMG_9658_2.jpg"
+
+
+    ),
+
+    new ChatModel(
+      name: "Nandini",
+      message: "Hi Hyderrr",
+      time: "15:30",
+      avatarUrl: "https://drive.google.com/open?id=1Ohy6is0qmu8kv5RsrFyz9PTpqiMJT9T8"
+
+
+    ),
+    new ChatModel(
+      name: "Rohit",
+      message: "Yoo dawggg",
+      time: "15:30",
+      avatarUrl: "https://drive.google.com/open?id=1Ohy6is0qmu8kv5RsrFyz9PTpqiMJT9T8"
+
+
+    ),
+     new ChatModel(
+      name: "Lolli TSPA",
+      message: "I'm a Monkey",
+      time: "15:30",
+      avatarUrl: "https://drive.google.com/open?id=1Ohy6is0qmu8kv5RsrFyz9PTpqiMJT9T8"
+
+
+    ),
+    new ChatModel(
+      name: "Chaitu",
+      message: "Okay Donee",
+      time: "15:30",
+      avatarUrl: "https://drive.google.com/open?id=1Ohy6is0qmu8kv5RsrFyz9PTpqiMJT9T8"
+
+
+    )
+  ];
+  
+
---
+++ lib/whatsapp_home.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:helloflutter/pages/call_screen.dart';
+import 'package:helloflutter/pages/camera_screen.dart';
+import 'package:helloflutter/pages/chat_screen.dart';
+import 'package:helloflutter/pages/status_screen.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome>
+ with SingleTickerProviderStateMixin{
+
+   TabController _tabController;
+
+   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController= new TabController(vsync: this,initialIndex: 1,length:4);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("WhatsApp"),
+        elevation: 0.7,
+        bottom: new TabBar(
+
+          controller: _tabController,
+          indicatorColor: Colors.white ,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.camera_alt)),
+            new Tab(text: "CALLS"),
+            new Tab(
+              text: "STATUS",
+              ),
+            new Tab(
+              text: "Chats",
+              ),
+          ],
+        ),
+        actions: <Widget>[
+          new Icon(Icons.search),new Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0)
+          ), new Icon(Icons.more_vert)
+        ],
+      ),
+    body: new TabBarView(
+      controller: _tabController,
+      children: <Widget>[
+
+        new CameraScreen(),
+        new ChatScreen(),
+        new StatusScreen(),
+        new CallsScreen(),
+      ],
+
+    ),
+    floatingActionButton: new FloatingActionButton(
+      backgroundColor: Theme.of(context).accentColor,
+      child: new Icon(Icons.message, color: Colors.white,
+      ),
+      onPressed: ()=> print("Open Chats"),
+    ),
+    );
+  }
+}
